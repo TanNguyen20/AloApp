a2f1d43b1b12cf0a1e4b93e2e568a971bace5c38
Phong2589 <phonghuynh2589@gmail.com>
2021-08-13 08:59:36 +0700

    phong commit

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const {v4: uuidV4} = require('uuid');
 const handlebars = require('express-handlebars');
+const port = 3000;
 
 app.engine(
     'hbs',
@@ -21,13 +22,21 @@ app.set('view engine', 'hbs') // Tell Express we are using EJS
 app.use(express.static(path.join(__dirname, 'public/')));
 app.set('views', path.join(__dirname, 'resources/views'));
 // If they join the base link, generate a random UUID and send them to a new room with said UUID
-app.get('/', (req, res) => {
-    res.redirect(`/${uuidV4()}`)
-})
+// app.get('/', (req, res) => {
+//     res.redirect(`/${uuidV4()}`)
+// })
 // If they join a specific room, then render that room
-app.get('/:room', (req, res) => {
-    res.render('videoChat/room', {roomId: req.params.room})
-})
+// app.get('/:room', (req, res) => {
+//     res.render('videoChat/room', {roomId: req.params.room})
+// })
+
+const route = require('./routes');
+route(app);
+
+// app.get('/', (req, res) => {
+//         res.render('signIn')
+//      })
+
 // When someone connects to the server
 io.on('connection', socket => {
     // When someone attempts to join the room
@@ -42,4 +51,7 @@ io.on('connection', socket => {
     })
 })
 
-server.listen(3000) // Run the server on the 3000 port
+//server.listen(3000) // Run the server on the 3000 port
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`);
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,7 @@
-const newsRouter = require('./news');
-const siteRouter = require('./site');
-const courceRouter = require('./cources');
-const meRouter = require('./me');
-function route(app) {
-    app.use('/me', meRouter);
-    app.use('/cources', courceRouter);
-    app.use('/new', newsRouter);
-    app.use('/', siteRouter);
+const homeRouter = require('./home');
 
+function route(app) {
+    app.use('/', homeRouter);
 }
 
 module.exports = route;
