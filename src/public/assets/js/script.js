957bb287ef2bf5157a926426675398de62f8b0a8
NienLuanNganhCT466 <nienluannganhct466@gmail.com>
2021-08-16 22:31:10 +0700

    fix add server stun

--- src/public/assets/js/script.js
+++ src/public/assets/js/script.js
@@ -1,7 +1,20 @@
 const socket = io('/') // Create our socket
 const videoGrid = document.getElementById('video-grid') // Find the Video-Grid element
 
-const myPeer = new Peer() // Creating a peer element which represents the current user
+const myPeer = new Peer({config:{iceServers: [{
+    urls: [ "stun:ss-turn1.xirsys.com" ]
+ }, {
+    username: "YX5ln94dST7Kw3Sfs8h6oO1GApjoofmAlOUA8BTPwLc3ShgqLQMlxL_75dpSqVeOAAAAAGEaYEluaWVubHVhbm5nYW5oY3Q0NjY=",
+    credential: "40ff7ba8-fe91-11eb-8e5b-0242ac140004",
+    urls: [
+        "turn:ss-turn1.xirsys.com:80?transport=udp",
+        "turn:ss-turn1.xirsys.com:3478?transport=udp",
+        "turn:ss-turn1.xirsys.com:80?transport=tcp",
+        "turn:ss-turn1.xirsys.com:3478?transport=tcp",
+        "turns:ss-turn1.xirsys.com:443?transport=tcp",
+        "turns:ss-turn1.xirsys.com:5349?transport=tcp"
+    ]
+ }]}}) // Creating a peer element which represents the current user
 const myVideo = document.createElement('video') // Create a new video tag to show our video
 myVideo.muted = true // Mute ourselves on our end so there is no feedback loop
 
